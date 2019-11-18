f1f91c1c0fa05950652a20fed5b56e082234b312
liuan <1761330769@qq.com>
2019-11-18 17:14:51 +0800

    ..

--- node-app/config/datebase.js
+++ node-app/config/datebase.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV == "production") {
   module.exports = {
-    mongoURL: "mongodb://mongodb+srv://alvis:8bR39mc9@cluster0-rzg8n.mongodb.net/test?retryWrites=true&w=majority"
+    mongoURL: "mongodb://mongodb+srv://alvis:<password>@cluster0-rzg8n.mongodb.net/test?retryWrites=true&w=majority"
   }
 } else {
   module.exports = {
